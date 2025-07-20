b313388d8794916238fe4c5d785b9276f9b49f4f
ursapictura <hsmithfreelance@gmail.com>
2025-07-20 16:37:02 -0500

    Fixed typo in Category on Pattern model.

--- MakerSpace/Models/Pattern.cs
+++ MakerSpace/Models/Pattern.cs
@@ -7,7 +7,7 @@
         public int MakerId { get; set; }
         public User Maker { get; set; }
         public int CategoryId { get; set; }
-        public Category Catergory { get; set; }
+        public Category Category { get; set; }
         public string Description { get; set; }
         public DateOnly Published { get; set; }
         public decimal Price { get; set; }
