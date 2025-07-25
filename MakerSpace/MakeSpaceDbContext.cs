08ec556c37b6286a841fff7084d17c3425bc4698
ursapictura <hsmithfreelance@gmail.com>
2025-07-24 19:13:57 -0500

    DbContext created and modelBUilder implemented.

---
+++ MakerSpace/MakeSpaceDbContext.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MakerSpace.Models;
+using MakerSpace.Data;
+
+namespace MakerSpace
+{
+    public class MakerSpaceDbContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Pattern> Patterns { get; set; }
+        public DbSet<Library> Libraries { get; set; }
+        public DbSet<Storefront> Storefronts { get; set; }
+        public DbSet<Tag> Tags { get; set; }
+        public DbSet<FavoritePattern> FavoritePatterns { get; set; }
+        public DbSet<LibraryPattern> LibraryPatterns { get; set; }
+        public DbSet<PatternTag> PatternTags { get; set; }
+
+        public MakerSpaceDbContext(DbContextOptions<MakerSpaceDbContext> options) : base(options) { }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<User>().HasData(UserData.Users);
+            modelBuilder.Entity<Category>().HasData(CategoryData.Categories);
+            modelBuilder.Entity<Pattern>().HasData(PatternData.Patterns);
+            modelBuilder.Entity<Library>().HasData(LibraryData.Libraries);
+            modelBuilder.Entity<Storefront>().HasData(StorefrontData.Storefronts);
+            modelBuilder.Entity<Tag>().HasData(TagData.Tags);
+            modelBuilder.Entity<FavoritePattern>().HasData(FavoritePatternData.FavoritePatterns);
+            modelBuilder.Entity<LibraryPattern>().HasData(LibraryPatternData.LibraryPatterns);
+            modelBuilder.Entity<PatternTag>().HasData(PatternTagData.PatternTags);
+        }
+    }
+}
--- MakerSpace/Program.cs
+++ MakerSpace/Program.cs
@@ -1,3 +1,7 @@
+using System.Text.Json.Serialization;
+using Microsoft.AspNetCore.Http.Json;
+using MakerSpace;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -14,7 +18,31 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
+builder.Services.AddCors(options =>
+{
+    options.AddDefaultPolicy(policy =>
+    {
+        policy.WithOrigins("http://localhost:3000")
+            .AllowAnyMethod()
+            .AllowAnyHeader();
+    });
+});
+
+// allows passing datetimes without time zone data 
+AppContext.SetSwitch("Npgsql.EnableLegacyTimestampBehavior", true);
+
+// allows our api endpoints to access the database through Entity Framework Core
+builder.Services.AddNpgsql<MakerSpaceDbContext>(builder.Configuration["MakerSpaceDbConnectionString"]);
+
+// Set the JSON serializer options
+builder.Services.Configure<JsonOptions>(options =>
+{
+    options.SerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles;
+});
+
 app.UseHttpsRedirection();
+
+app.UseCors();
 
 
 app.Run();
