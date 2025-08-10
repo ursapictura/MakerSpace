c95fe300c2bad634ee60cf90414bc77f198ee3ee
ursapictura <hsmithfreelance@gmail.com>
2025-08-10 16:24:34 -0500

    API calls for Patterns, Categories, Tags, and PatternTags created.

---
+++ MakerSpace/Endpoints/CategoryEndpoints.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MakerSpace.Models;
+
+namespace MakerSpace.Endpoints
+{
+    public static class CategoryEndpoints
+    {
+        public static void MapCategoryEndpoints(this IEndpointRouteBuilder routes)
+        {
+            var group = routes.MapGroup("/api/category").WithTags(nameof(Category));
+
+            routes.MapGet("", async (MakerSpaceDbContext db) =>
+            {
+                var categories = await db.Categories.ToListAsync();
+
+                if (categories.Count == 0) 
+                {
+                    return Results.NoContent();
+                }
+
+                return Results.Ok(categories);
+            });
+        }
+    }
+}
